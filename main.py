f56617e57397e605117dd219b63b1ada3d4d004b
k4zoUhq <rxus91@outlook.fr>
2026-06-15 14:35:48 +0200

    main.py

--- main.py
+++ main.py
@@ -10,12 +10,21 @@ LIEN_SITE = "https://nude-snap.cc/join"
 
 async def handle_join_request(update, context):
     user = update.chat_join_request.from_user
+
     bouton = InlineKeyboardMarkup([[
         InlineKeyboardButton("👉 Cliquez ici", url=LIEN_SITE)
     ]])
+
     await context.bot.send_message(
         chat_id=user.id,
-        text=f"Bonjour {user.first_name} 👋\n\nPour accéder au canal, inscris-toi ici :",
+        text=(
+            f"Bonjour {user.first_name} 👋\n\n"
+            "Pour accéder au canal, vous devez obligatoirement :\n\n"
+            "🔞 Avoir 21 ans ou plus.\n"
+            "📧 Utiliser une adresse e-mail valide lors de l'inscription.\n\n"
+            "Une fois votre inscription effectuée et validée, votre accès au canal sera accepté automatiquement.\n\n"
+            "Cliquez ci-dessous pour vous inscrire :"
+        ),
         reply_markup=bouton
     )
 
