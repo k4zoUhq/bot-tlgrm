11774c7336feddc0c816231b9dec66a72b6b52c6
k4zoUhq <rxus91@outlook.fr>
2026-06-15 14:14:03 +0200

    main.py

--- main.py
+++ main.py
@@ -1,31 +1,26 @@
 import os
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Application, ChatJoinRequestHandler, ContextTypes
+import logging
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Application, ChatJoinRequestHandler
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8634045765:AAFE_nXAc1XpX_lXQpgip0Cje8xqfkjfc1w")
-LIEN_SITE = "https://nude-snap.cc/join"  # Change ça !
+logging.basicConfig(level=logging.INFO)
 
-async def handle_join_request(update: Update, context: ContextTypes.DEFAULT_TYPE):
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+LIEN_SITE = "https://nude-snap.cc/join"
+
+async def handle_join_request(update, context):
     user = update.chat_join_request.from_user
-    
     bouton = InlineKeyboardMarkup([[
-        InlineKeyboardButton("👉 S'inscrire ici", url=LIEN_SITE)
+        InlineKeyboardButton("👉 Cliquez ici", url=LIEN_SITE)
     ]])
-    
     await context.bot.send_message(
         chat_id=user.id,
-        text=(
-            f"Bonjour {user.first_name} 👋\n\n"
-            f"Pour accéder au canal, tu dois d'abord t'inscrire sur notre site :\n\n"
-            f"Une fois inscrit, ton accès sera validé automatiquement ✅\n\n"
-            f"Pour être accepter, au moment de l'inscriptions tu doit avoir obligatoirement plus de 21 ans ou + et mettre une e-mail valide ! 🍓"
-        ),
+        text=f"Bonjour {user.first_name} 👋\n\nPour accéder au canal, inscris-toi ici :",
         reply_markup=bouton
     )
 
-app = Application.builder().token(BOT_TOKEN).build()
-app.add_handler(ChatJoinRequestHandler(handle_join_request))
-
-print("Bot démarré !")
-import asyncio
-asyncio.run(app.run_polling())
+if __name__ == "__main__":
+    app = Application.builder().token(BOT_TOKEN).build()
+    app.add_handler(ChatJoinRequestHandler(handle_join_request))
+    print("Bot démarré !")
+    app.run_polling()
